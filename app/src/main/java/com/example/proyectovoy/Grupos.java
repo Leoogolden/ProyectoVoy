01794ac8d561140de318b2fe91cc0da2f4028ee9
Android Studio <devandroid@ort>
2019-06-21 12:10:19 -0300

    keloke

---
+++ app/src/main/java/com/example/proyectovoy/Grupos.java
@@ -0,0 +1,13 @@
+package com.example.proyectovoy;
+
+public class Grupos {
+    public int IdGrupo;
+    public String Nombre;
+    public String Descripcion;
+
+    Grupos(int a, String b, String c) {
+        IdGrupo = a;
+        Nombre = b;
+        Descripcion = c;
+    }
+}
--- app/src/main/java/com/example/proyectovoy/MisGrupos.java
+++ app/src/main/java/com/example/proyectovoy/MisGrupos.java
@@ -1,10 +1,12 @@
 package com.example.proyectovoy;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,22 +15,37 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.example.proyectovoy.Grupos;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class MisGrupos extends AppCompatActivity {
-    String[] listviewTitle = new String[]{
-            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4"
-    };
+
+    List<String> listagrupos;
+    ArrayList<Grupos> ListaDeGrupos = new ArrayList<Grupos>();
     int[] listviewImage = new int[]{
             R.drawable.ejemploperfilgrupo, R.drawable.ejemploperfilgrupo, R.drawable.ejemploperfilgrupo, R.drawable.ejemploperfilgrupo
     };
+
+    int idUsr = 2;
+    ArrayList<String> results = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_grupos);
+        tareaAsincronica miTarea = new tareaAsincronica();
+        miTarea.execute();
         BottomNavigationView bNavView = (BottomNavigationView) findViewById(R.id.navigation_view);
         Menu menunav = bNavView.getMenu();
         MenuItem itemseleccionado = menunav.getItem(2);
@@ -59,27 +76,106 @@ public class MisGrupos extends AppCompatActivity {
                 return false;
             }
         });
-        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
-        for (int i = 0; i < 4; i++) {
-            HashMap<String, String> hm = new HashMap<String, String>();
-            hm.put("listview_title", listviewTitle[i]);
-            hm.put("listview_image", Integer.toString(listviewImage[i]));
-            aList.add(hm);
-        }
-        String[] from = {"listview_image", "listview_title"};
-        int[] to = {R.id.listview_image, R.id.listview_item_title};
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.activity_listagrupos, from, to);
-        ListView androidListView = (ListView) findViewById(R.id.ListaGrupos);
-        Log.d("Holahola", "Llegue chicos");
-        androidListView.setAdapter(simpleAdapter);
-
 
     }
-public void ClickedBtn(){
-        Intent asd;
-        asd = new Intent(this, InfoGrupo.class);
-        startActivity(asd);
+
+
+
+    private class tareaAsincronica extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL rutatlantica = new URL("http://10.152.2.21:2073/api/Grupos/" + idUsr);
+                HttpURLConnection conexion = (HttpURLConnection) rutatlantica.openConnection();
+                Log.d("AccesoAPI", "Me conecto");
+                if (conexion.getResponseCode() == 200) {
+                    Log.d("AccesoAPI", "conexion ok");
+                    InputStream cuerporesspuesta = conexion.getInputStream();
+                    InputStreamReader lectorrespuesta = new InputStreamReader(cuerporesspuesta, "UTF-8");
+                    ProcessJSONLeido(lectorrespuesta);
+                } else {
+                    Log.d("AccesoAPI", "Error en la conexion");
+                }
+            } catch (Exception error) {
+                Log.d("AccesoAPI", "Huno un error al conectarme" + error.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d("HolaHola", "ueso, que pasoa");
+            List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+            Log.d("HolaHola", "ueso, que pasoa2");
+            int lenght = ListaDeGrupos.size();
+            for (int i = 0; i < lenght; i++) {
+                Log.d("HolaHola", "ueso, que pasoa3" + ListaDeGrupos.get(i).Nombre);
+                HashMap<String, String> hm = new HashMap<String, String>();
+                hm.put("listview_title", ListaDeGrupos.get(i).Nombre);
+                hm.put("listview_image", Integer.toString(listviewImage[i]));
+                Log.d("HolaHola", "ueso, que pasoa4");
+                aList.add(hm);
+            }
+            Log.d("HolaHola", "ueso, que pasoa5");
+            String[] from = {"listview_image", "listview_title"};
+            int[] to = {R.id.listview_image, R.id.listview_item_title};
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.activity_listagrupos, from, to);
+            ListView androidListView = (ListView) findViewById(R.id.ListaGrupos);
+            Log.d("Holahola", "Llegue chicos");
+            androidListView.setAdapter(simpleAdapter);
+        }
+    }
+
+    public void ProcessJSONLeido(InputStreamReader streamLeido) {
+
+        JsonReader JSONleido = new JsonReader(streamLeido);
+        try {
+            JSONleido.beginArray();
+            while (JSONleido.hasNext()) {
+                Log.d("uoso", "1");
+                JSONleido.beginObject();
+                Log.d("uoso", "2");
+                int idGrupo = 0;
+                String nombre = "", descripcion = "";
+                while (JSONleido.hasNext()) {
+                    String NomeDuElemento = JSONleido.nextName();
+                    Log.d("uoso", "3");
+                    if (NomeDuElemento.equals("IdGrupo")) {
+                        Log.d("uoso", "4");
+                        idGrupo = JSONleido.nextInt();
+                    } else if (NomeDuElemento.equals("Nombre")) {
+                        Log.d("uoso", "5");
+                        nombre = JSONleido.nextString();
+                    } else if (NomeDuElemento.equals("Descripcion")) {
+                        Log.d("uoso", "6");
+                        descripcion = JSONleido.nextString();
+                    }
+                }
+                Grupos asd = new Grupos(idGrupo, nombre, descripcion);
+//                Log.d("uoso", ""+ asd);
+                ListaDeGrupos.add(asd);
+                JSONleido.endObject();
+            }
+            JSONleido.endArray();
+
+
+        } catch (Exception error) {
+            Log.d("LecturaJSON", "" + error);
+        }
+
+    }
+
+
+
+
+
+
+    public void ClickedBtn() {
+        Intent asd1;
+        asd1 = new Intent(this, InfoGrupo.class);
+        startActivity(asd1);
 
     }
 
