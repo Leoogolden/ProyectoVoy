99da45709720a51cbb5e9de5c960967ebd97d5be
Android Studio <devandroid@ort>
2019-10-18 09:42:24 -0300

    lol

--- app/src/main/java/com/example/proyectovoy/SelectedEvent.java
+++ app/src/main/java/com/example/proyectovoy/SelectedEvent.java
@@ -1,6 +1,7 @@
 package com.example.proyectovoy;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 
 public class SelectedEvent extends Fragment {
@@ -20,20 +27,28 @@ public class SelectedEvent extends Fragment {
     Grupos grupaso = new Grupos();
     Bundle DatosRecibidos;
     Bundle GrupoElegido;
-Bundle Activ;
-    int idGrupo;
+    Bundle Activ;
+
+    Boolean esmiembro = false;
+
+    Button ConfirmarAsistencia;
+    Button UnirseAlGrupo;
     int idactividad;
 
     public View onCreateView(LayoutInflater inflador, ViewGroup grupo, Bundle datos) {
         vistadevuelve = inflador.inflate(R.layout.fragment_selected_event, grupo, false);
+
+        VerificarMiembro a = new VerificarMiembro();
+        a.execute();
+        ConfirmarAsistencia = vistadevuelve.findViewById(R.id.BotonConfirmar);
+        UnirseAlGrupo = vistadevuelve.findViewById(R.id.UnirseAlGrupo);
+        UnirseAlGrupo.setVisibility(View.GONE);
 
         DatosRecibidos = getArguments();
         GrupoElegido = DatosRecibidos.getBundle("grupaso");
         String Nombre = GrupoElegido.getString("Nombre");
         String Descripcion = GrupoElegido.getString("Descripcion");
         Log.d("onclick", "entra4");
-        idGrupo = GrupoElegido.getInt("idGrupo");
-        grupaso.IdGrupo = idGrupo;
         grupaso.Nombre = Nombre;
         grupaso.Descripcion = Descripcion;
 
@@ -59,11 +74,10 @@ Bundle Activ;
         activ.setFechaActiv((Date) Activ.getSerializable("FechaAct"));
 
 
-        Button ConfirmarAsistencia = vistadevuelve.findViewById(R.id.BotonConfirmar);
-        Button UnirseAlGrupo = vistadevuelve.findViewById(R.id.UnirseAlGrupo);
         UnirseAlGrupo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
             }
         });
@@ -76,4 +90,49 @@ Bundle Activ;
 
 
         return vistadevuelve;
-    }}
+    }
+
+    private class VerificarMiembro extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Log.d("AccesoAPI6", "aaaa");
+                URL rutatlantica2 = new URL(R.string.IP + "Grupos/Estaono/" + user.IdUsuario + "/" + grupaso.IdGrupo);
+                Log.d("qonda", "aa " + rutatlantica2);
+                HttpURLConnection conexion = (HttpURLConnection) rutatlantica2.openConnection();
+                Log.d("AccesoAPI6", "Me conecto");
+                if (conexion.getResponseCode() == 200) {
+                    Log.d("AccesoAPI6", "conexion ok");
+                    InputStream cuerporesspuesta = conexion.getInputStream();
+                    InputStreamReader lectorrespuesta = new InputStreamReader(cuerporesspuesta, "UTF-8");
+                    ProcesaMiembro(lectorrespuesta);
+                } else {
+                    Log.d("AccesoAPI6", "Error en la conexion " + conexion.getResponseCode());
+                }
+                conexion.disconnect();
+            } catch (Exception error) {
+                Log.d("AccesoAPI6", "Huno un error al conectarme" + error.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d("qonda", "es admin " + esmiembro.toString());
+            if (esmiembro) {
+                UnirseAlGrupo.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void ProcesaMiembro(InputStreamReader streamLeido) {
+        JsonParser parseador;
+        parseador = new JsonParser();
+
+        esmiembro = parseador.parse(streamLeido).getAsBoolean();
+
+    }
+
+
+}
